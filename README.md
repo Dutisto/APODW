12b9a6a02b8ea4e8dfa2abfe0a5dbd1f7cdad48b
axel <216248@supinfo.com>
2018-03-31 11:47:01 +0200

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # BashApod
+
+A simple service based on Astronomy Picture of the Day by the NASA (https://apod.nasa.gov/apod/astropix.html)
+
+The service check every hour if a new picture is available, if so download it and set it as your new wallpaper.
+
+Installation :
+
+Simply run sudo install.sh with the username of the user you want to run the service as. (You will want to set it as the current user)
+
+sudo install.sh dutisto
---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Installing necessary tools..."
+
+apt-get install jq curl &>/dev/null
+
+sed -i 's/USER/'"$1"'/g' apodw.service
+
+echo "Installing service..."
+
+cp apodw.service /etc/systemd/system/
+cp apodw.timer /etc/systemd/system/
+
+cp apodw /usr/bin/apodw
+
+systemctl enable apodw.service
+systemctl enable apodw.timer
+
+echo "Starting service..."
+
+service apodw start || echo "something went wrong during APOD start up =["
